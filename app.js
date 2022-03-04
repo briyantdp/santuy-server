264fb36a8a181dc74865d7a38f18a681781687cf
Bryant Dawson Priyantoro <briyantazazel7@gmail.com>
2022-03-04 11:10:31 +0700

    Edit mongo atlas URL

--- app.js
+++ app.js
@@ -5,23 +5,26 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
-const flash = require('connect-flash');
-const session = require('express-session')
-const cors = require('cors');
+const flash = require("connect-flash");
+const session = require("express-session");
+const cors = require("cors");
 
-mongoose.connect('mongodb+srv://bryant:santuy@cluster0.uxdim.mongodb.net/db_santuy?retryWrites=true&w=majority', {  
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  "mongodb+srv://bryant:azazel789@cluster0.uxdim.mongodb.net/db_santuy?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 // Router admin
 const adminRouter = require("./routes/admin");
 // Router API
-const apiRouter = require("./routes/api")
+const apiRouter = require("./routes/api");
 
 var app = express();
 
@@ -29,15 +32,17 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { 
-    maxAge : 60000,
-    sameSite : "lax",
-  }
-}));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 60000,
+      sameSite: "lax",
+    },
+  })
+);
 app.use(flash());
 app.use(logger("dev"));
 app.use(express.json());
@@ -56,8 +61,6 @@ app.use(cors());
 // Endpoint admin
 app.use("/admin", adminRouter);
 app.use("/api/v1/customer", apiRouter);
-
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
